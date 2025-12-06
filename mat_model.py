32590e2c1b3eb11805549d563a117fc7bee42e0f
ekaaaaaat <18be12@mail.ru>
2025-12-06 21:49:23 +0300

    add initial parameters

--- mat_model.py
+++ mat_model.py
@@ -1,3 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+
+# const
+
+R = 8.31
+A1 = 92 * 10**8
+E1 = 150000
+A2 = 16*10**7
+E2 = 132000
+A3 = 580
+E3 = 60000
+D = 0.12
+T = 640
+m_c1 = 0.015
+m_O2 = 0.035
+ro = 200
+mu_C1 = 0.078  # кг/моль
+mu_C2 = 0.098  # кг/моль
+
+L = 5
+d_l = 0.01
