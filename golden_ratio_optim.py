5a4d6158a9d8ccfcce3340e849d716e729a23838
ekaaaaaat <18be12@mail.ru>
2025-12-10 23:56:46 +0300

    add function(type_of_extremum) to determine the type of extremum

--- golden_ratio_optim.py
+++ golden_ratio_optim.py
@@ -17,3 +17,11 @@ def negative_f(x):
     # Функция с противоположным знаком (если экстремум максимум)
     return -(1 - x**2)
 
+
+def type_of_extremum(a_, b_, f_):
+    c = (a_ + b_) / 2
+    if f_(a_) > f_(c) or f_(b_) > f_(c):
+        type_ex = 'минимум'
+    else:
+        type_ex = 'максимум'
+    return type_ex
