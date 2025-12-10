b1bffffa825b0582ba524c469371b4affeb5d6a6
ekaaaaaat <18be12@mail.ru>
2025-12-11 00:16:42 +0300

    add function to visualize golden ratio method work

--- golden_ratio_optim.py
+++ golden_ratio_optim.py
@@ -67,6 +67,27 @@ def golden_ratio(a_, b_, f_, eps_):
     return ak, bk, x_list
 
 
+def graf(a_, b_, f_, ak, bk, x):
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.grid()
+    k = np.linspace(a_ - 0.1, b_ + 0.1, 1000)
+    plt.plot(k, f_(k), color='purple', linewidth=2.0, label='Целевая функция')
+    plt.plot([x[0], x[0]], [f_(x[0]), -5], color='g', ls='--', linewidth=2.0, marker='x', markersize=7, label='x*')
+    plt.plot([ak[0], ak[0]], [f_(ak[0]), -5], color='hotpink', ls='--', linewidth=1.0, marker='o', markersize=4, label='ak')
+    plt.plot([bk[0], bk[0]], [f_(bk[0]), -5], color='aqua', ls='--', linewidth=1.0, marker='o', markersize=4, label='bk')
+
+    for j in range(1, len(x)):
+        plt.plot([x[j], x[j]], [f_(x[j]), -5], color='g', ls='--', linewidth=2.0, marker='x', markersize=7)
+        plt.plot([ak[j], ak[j]], [f_(ak[j]), -5], color='hotpink', ls='--', linewidth=1.0, marker='o', markersize=4)
+        plt.plot([bk[j], bk[j]], [f_(bk[j]), -5], color='aqua', ls='--', linewidth=1.0, marker='o', markersize=4)
+    plt.legend()
+
+    plt.plot(x[-1], f_(x[-1]), color='red', linewidth=3.0, marker='o', markersize=10)
+
+    plt.show()
+
+
 type_ex = type_of_extremum(a0, b0, f)
 print('Тип экстремума: ' + type_ex)
 if type_ex == 'минимум':
@@ -79,3 +100,4 @@ table = []
 for i in range(len(x_values)):
     table.append([i, ak_values[i], bk_values[i], x_values[i]])
 print(tabulate(table, headers=["Итерация", "a", "b", "x"], tablefmt="pretty"))
+graf(a0, b0, f, ak_values, bk_values, x_values)
