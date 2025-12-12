7afb03b54cc124eb206b26cc81bb885ec183fb16
ekaaaaaat <18be12@mail.ru>
2025-12-12 17:08:34 +0300

    Add initial parameters of the random process and model

--- simulation_modeling.py
+++ simulation_modeling.py
@@ -1,3 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+
+lambda1 = math.pow(5, 11)
+lambda2 = math.pow(2, 18)
+M0 = 770
+sigma0_2 = 400
+alpha0 = 0.09
+N = 210
+Kz_num = 20
+Z0 = 1
+
+A1 = 1
+A2 = 1
+Ns = 10
