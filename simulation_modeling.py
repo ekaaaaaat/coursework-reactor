f9172287d226a5781d7da453d386e880068c6ba3
ekaaaaaat <18be12@mail.ru>
2025-12-12 18:11:41 +0300

    Add function to approximate the values of the correlation function

--- simulation_modeling.py
+++ simulation_modeling.py
@@ -52,6 +52,24 @@ def correlation_function(z, M, Smax):
     return K
 
 
+# Аппроксимация
+def approximate_alpha(S, K, sig_2, max_iter=1000):
+    alph = []
+    for i in range(len(S)):
+        alp = 1
+        kt = sig_2 * math.exp(-alp * abs(S[i]))
+        iter_count = 0
+        while kt < abs(K[i]) and iter_count < max_iter:
+            alp -= 0.1
+            kt = sig_2 * math.exp(-alp * abs(S[i]))
+            iter_count += 1
+        alph.append(max(alp, 0.001))  # не даем alpha быть отрицательной
+
+    alph_final = sum(alph) / len(alph)
+    k_appr = [sig_2 * math.exp(-alph_final * abs(S[i])) for i in range(len(S))]
+    return alph_final, k_appr
+
+
 def generate_process_z(x, A1, A2, sigma0_2, sigma_x0_2, alpha0, M0, Ns):
     # Функция генерации случайного процесса
     z = []
@@ -88,10 +106,13 @@ print(z)
 Mz = checkmate_waiting(z)
 sigma_z2 = dispersion(z, Mz)
 Kz = correlation_function(z, Mz, Kz_num)
-print("Kz", Kz)
+alpha_z, K_apr = approximate_alpha(range(Kz_num), Kz, sigma_z2)
+# print("Kz", Kz)
 
 # ВЫВОД1
 print(f"Mx = {Mx:.4f}")
 print(f"sigma_x^2 = {sigma_x2:.4f}")
 print(f"Mz = {Mz:.4f}")
 print(f"sigma_z^2 = {sigma_z2:.4f}")
+print(f"alpha_z = {alpha_z:.4f}")
+print(f"A1 = {A1:.4f}, A2 = {A2:.4f}")
